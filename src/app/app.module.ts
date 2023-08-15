1edcd833fb506b076563f4dfcf4fe6267fc6889f
liadber <liadbercovich@gmail.com>
2023-08-15 20:04:58 +0300

    Fix: profile header margin for grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { GridComponent } from './common/grid/grid.component';
-import { GridCellComponent } from './common/grid/grid-cell/grid-cell.component';
-import { ProfileComponent } from './common/profile/profile.component';
-import { ProfileHeaderComponent } from './common/profile/profile-header/profile-header.component';
-import { ProjectComponent } from './common/project/project.component';
-import { GalleryComponent } from './common/project/gallery/gallery.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {ProjectsComponent} from './projects/projects.component';
+import {GridComponent} from './common/grid/grid.component';
+import {GridCellComponent} from './common/grid/grid-cell/grid-cell.component';
+import {ProfileComponent} from './common/profile/profile.component';
+import {ProfileHeaderComponent} from './common/profile/profile-header/profile-header.component';
+import {ProjectComponent} from './common/project/project.component';
+import {ProjectGalleryComponent} from './common/project/project-gallery/project-gallery.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +21,7 @@ import { GalleryComponent } from './common/project/gallery/gallery.component';
     ProfileComponent,
     ProfileHeaderComponent,
     ProjectComponent,
-    GalleryComponent,
+    ProjectGalleryComponent,
   ],
   imports: [
     BrowserModule
@@ -29,4 +29,5 @@ import { GalleryComponent } from './common/project/gallery/gallery.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
