863c26d29d6dd6a6fbef76ce8fdbbae366a4c98e
liadber <liadbercovich@gmail.com>
2023-08-16 15:50:29 +0300

    Fix: trying to strech

---
+++ src/app/common/project/project-gallery/project-gallery.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-gallery',
+  templateUrl: './project-gallery.component.html',
+  styleUrls: ['./project-gallery.component.css']
+})
+export class ProjectGalleryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
