41070327406531e98adde1a7c34e53983b3ff871
liadber <liadbercovich@gmail.com>
2023-09-23 21:21:17 +0300

    feature: flexible Project's page

--- src/app/common/project/project.component.ts
+++ src/app/common/project/project.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-project',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectComponent implements OnInit {
 
-  constructor() { }
+  @Input() editMode: boolean = true;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
