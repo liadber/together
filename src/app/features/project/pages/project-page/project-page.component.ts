c9a29add197ced12ca7d2736a340f3ca01e4290f
liadber <liadbercovich@gmail.com>
2023-11-04 05:44:52 +0200

    feature: project-page as Reactive Form

--- src/app/features/project/pages/project-page/project-page.component.ts
+++ src/app/features/project/pages/project-page/project-page.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {FormBuilder} from '@angular/forms';
+import {FormArray, FormBuilder} from '@angular/forms';
 import {Validators} from '@angular/forms';
 
 @Component({
@@ -14,13 +14,44 @@ export class ProjectPageComponent implements OnInit {
   projectForm = this.fb.group({
     projectName: ['New Album', Validators.required],
     projectDescription: ['Hey everyone...\n' +
-    '          I’m a singer in my beginning. Looking for a cool producer to do some shit together.\n' +
-    '           I want to make a song+clip to make a breakthrough.\n' +
-    '           I’m looking for beginners semi-professionals to start an interesting way together. <br>I’m a singer in my\n' +
-    '          beginning. Looking for a cool producer to do some shit together.\n'],
+    'I’m a singer in my beginning. Looking for a cool producer to do some shit together.\n' +
+    'I want to make a song+clip to make a breakthrough.\n' +
+    'I’m looking for beginners semi-professionals to start an interesting way together.\n' +
+    'I’m a singer in my beginning.\n' +
+    'Looking for a cool producer to do some shit together.\n' +
+    'I’m a singer in my beginning. Looking for a cool producer to do some shit together.\n' +
+    'I want to make a song+clip to make a breakthrough.\n' +
+    'I’m looking for beginners semi-professionals to start an interesting way together.\n' +
+    'I’m a singer in my beginning.\n' +
+    'Looking for a cool producer to do some shit together.\n' +
+    'I’m a singer in my beginning. Looking for a cool producer to do some shit together.\n' +
+    'I want to make a song+clip to make a breakthrough.\n' +
+    'I’m looking for beginners semi-professionals to start an interesting way together.\n' +
+    'I’m a singer in my beginning.\n' +
+    'Looking for a cool producer to do some shit together.\n'
+    ],
     projectPicture: [],
-    inDemand: ['', '', '']
+    inDemand: this.fb.array([
+      this.fb.control('Producer'),
+      this.fb.control('Record studio owner'),
+      this.fb.control('Guitarist'),
+      this.fb.control('Record company'),
+      this.fb.control('Fashionista'),
+      this.fb.control('Makeup artist'),
+      this.fb.control('Decorator'),
+      this.fb.control('Producer'),
+      this.fb.control('Record studio owner'),
+      this.fb.control('Guitarist'),
+      this.fb.control('Record company'),
+      this.fb.control('Fashionista'),
+      this.fb.control('Makeup artist'),
+      this.fb.control('Decorator'),
+    ])
   });
+
+  get inDemand() {
+    return this.projectForm.get('inDemand') as FormArray;
+  }
 
   constructor(private fb: FormBuilder) {
   }
