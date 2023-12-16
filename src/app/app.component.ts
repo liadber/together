6f5b37f9f482651deb8e780442a0f8d503a36f31
liadber <liadbercovich@gmail.com>
2023-12-16 07:50:35 +0200

    Refactor: Project Files Hierarchy for Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterOutlet} from '@angular/router';
 import {ProjectPageComponent} from "./features/project/pages/project-page/project-page.component";
-import {NavbarComponent} from "./core/navbar/navbar.component";
+import {NavbarComponent} from "./core/components/navbar/navbar.component";
 
 
 @Component({
