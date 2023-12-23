453c9043c18e727ee1a501c4d9ed1ab8b1a49938
liadber <liadbercovich@gmail.com>
2023-12-23 15:23:59 +0200

    Fix: Ngrx Devtool configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -19,7 +19,7 @@ export const appConfig: ApplicationConfig = {
     provideStoreDevtools({ //todo: delete after debug
       maxAge: 25, // Retains last 25 states
       logOnly: !isDevMode(), // Restrict extension to log-only mode
-      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      autoPause: false, // autoPause - Pauses recording actions and state changes when the extension window is not open
       trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
       traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
       connectInZone: true // If set to true, the connection is established within the Angular zone
